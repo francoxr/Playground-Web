f7053ca63fea163b38fdf1e2eb3c600c93502957
francoxr <franco.roca.pe@gmail.com>
2022-01-23 12:30:01 -0500

    add signal for create profile when user create a account

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,3 +9,12 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    """Signal , esta funcion se activara cuando se cree un usuario
+    se crearar un perfil para ese usuario."""
+
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(user=instance)
